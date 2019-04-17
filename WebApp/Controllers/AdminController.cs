86ae837b4203c1c1226fc6a97b6ff6ea72108db8
Thomasm666 <crusialgamer1@gmail.com>
2019-04-17 14:25:57 +0200

    Update AdminController.cs

--- WebApp/Controllers/AdminController.cs
+++ WebApp/Controllers/AdminController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 /// <summary>
-/// Httpget API admin
+/// Httpget API admin orders viewmodel
 /// </summary>
 namespace OrderFoodApp.WebApp.Controllers
 {
